2373b514547e430c984df46085795ba4a56d2092
pustart <polaris9955@gmail.com>
2023-04-04 20:42:40 +0300

    feat: add global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ru">
-        <Head />
+        <Head>
+        </Head>
         <body>
           <Main />
           <NextScript />
