1a7b17f188e6db9c7824bf1aa8ded8aa02a5f3f9
pustart <polaris9955@gmail.com>
2023-04-18 23:56:50 +0300

    refactor: update code style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
-import { AppProps } from 'next/dist/shared/lib/router/router';
-import '../styles/globals.css';
-import Head from 'next/head';
-import React from 'react';
-
+import { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/globals.css";
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <>
-    <Head>
-      <title>MoneyPie — приложение для ведение бюджета</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="icon" href="/images/favicons/logo-round.svg" type="image/svg+xml" />
-      <link rel="apple-icon" href="/images/favicons/apple-icon.png" />
-      <link rel="manifest" href="/manifest.webmanifest" />
-    </Head>
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>MoneyPie — приложение для ведение бюджета</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicons/logo-round.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/images/favicons/apple-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
