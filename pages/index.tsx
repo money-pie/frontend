607394ed1fc962dd08695ae53e27cad45b1ffd0d
userkatrishaa <borekat36@gmail.com>
2023-05-05 00:48:16 +0300

    feat: add heading component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Htag from "../components/Htag/Htag";
 
 export default function Home(): JSX.Element {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Htag tag="h1">MoneyPie</Htag>
+      <Htag tag="h2">Цель на месяц</Htag>
+      <Htag tag="h3">Продукты</Htag>
+      <Htag tag="h4">Подсказка</Htag>
+    </div>
+  );
 }
