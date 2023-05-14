82199dc02334e0b95e243bfe3b33ab1eb8677b15
pustart <polaris9955@gmail.com>
2023-05-15 01:21:14 +0300

    fix: add responsive layout for info modal window

--- components/modules/DetailedInformation/DetailedInformation.tsx
+++ components/modules/DetailedInformation/DetailedInformation.tsx
@@ -20,38 +20,43 @@ function DetailedInformation({ active, onClose }: DetailedInformationProps): JSX
         Подробная информация
       </Htag>
       <div className={styles.columns}>
-        <div className={styles["column-wrapper"]}>
-          <div className={styles.column}>
-            <div className={styles["column-image"]}>
-              <Image src="/images/person.png" alt="Иконка пользователя" width={160} height={160} />
-            </div>
-            <div className={styles["text-column"]}>
-              <p>
-                <span style={{ color: "var(--accent-weak)" }}>Тут имя из БД</span>
-              </p>
-              <p>добавляет</p>
-            </div>
+        <div className={styles.rows}>
+          <div className={styles["img-wrap"]}>
+            <Image
+              src="/images/person.png"
+              alt="Иконка категории: продукты"
+              width={160}
+              height={160}
+            />
           </div>
-          <div className={styles.column}>
-            <div className={styles["column-image"]}>
-              <Image
-                src="/images/product.png"
-                alt="Иконка категории: продукты"
-                width={160}
-                height={160}
-              />
-            </div>
-            <div className={styles["text-column"]}>
-              <p>
-                <span style={{ color: "var(--accent-weak)" }}>Категория:</span> из БД
-              </p>
-              <p>
-                <span style={{ color: "var(--accent-weak)" }}>Дата и время:</span> из БД
-              </p>
-              <p>
-                <span style={{ color: "var(--accent-weak)" }}>Описание:</span> из БД
-              </p>
-            </div>
+          <div className={styles.description}>
+            <p className={styles.text}>Василий Петрович</p>
+            <p className={styles.info}>добавляет</p>
+          </div>
+        </div>
+
+        <div className={styles.rows}>
+          <div className={styles["img-wrap"]}>
+            <Image
+              src="/images/product.png"
+              alt="Иконка категории: продукты"
+              width={160}
+              height={160}
+            />
+          </div>
+          <div className={styles.description}>
+            <Htag className={styles.info} tag="h3">
+              Категория
+            </Htag>
+            <p className={styles.text}>Продукты</p>
+            <Htag className={styles.info} tag="h3">
+              Дата и время
+            </Htag>
+            <p className={styles.text}>20 февраля</p>
+            <Htag className={styles.info} tag="h3">
+              Описание
+            </Htag>
+            <p className={`${styles.text} ${styles["rich-text"]}`}>Lorem Ipsum</p>
           </div>
         </div>
       </div>
