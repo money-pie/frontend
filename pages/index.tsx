130273dac8c5d88b812270cd7a576c5e9232863a
userkatrishaa <borekat36@gmail.com>
2023-05-17 22:28:06 +0300

    fix: update index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,16 @@
 import Head from "next/head";
 import { useState } from "react";
-import Htag from "../components/elements/Htag/Htag";
-import Button from "../components/elements/Button/Button";
-import Input from "../components/elements/Input/Input";
-import ExpenseAndIncomeWindow from "../components/modules/ExpenseAndIncomeWindow/ExpenseAndIncomeWindow";
+import { withLayout } from "../layout/Layout";
+import { MenuItem } from "../page-components/MenuItem/MenuItem";
+import { MainTarget } from "../page-components/MainTarget/MainTarget";
 
-export default function Home(): JSX.Element {
-  const [showModal, setShowModal] = useState<boolean>(false);
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
+function Home(): JSX.Element {
   return (
-    <div>
-      <Htag tag="h1">MoneyPie</Htag>
-      <Htag tag="h2">Цель на месяц</Htag>
-      <Htag tag="h3">Продукты</Htag>
-      <Htag tag="h4">Подсказка</Htag>
-
-      <Input appearance="normal" placeholder="Сумма" />
-
-      <Button appearance="premium" className="rounded" btnType="button">
-        Купить
-      </Button>
-      <Button appearance="ghost" className="rounded" btnType="button">
-        Статистика
-      </Button>
-      <Button appearance="casual" className="rounded" btnType="button">
-        Установить
-      </Button>
-      <Button appearance="ordinary" className="rounded" btnType="button">
-        Да
-      </Button>
-      <Button
-        appearance="ordinary"
-        className="rounded"
-        btnType="button"
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Проверить модальное окно
-      </Button>
-      {/* <QuestionModalWindow active={showModal} onClose={closeModal} text="сменить имя пользователя?"/> */}
-      {/* <SubscriptionBanner active={showModal} onClose={closeModal} /> */}
-      {/* <SubscriptionRenewal active={showModal} onClose={closeModal} /> */}
-      {/* <TargetAdding active={showModal} onClose={closeModal} /> */}
-      {/* <AutorizationForm active={showModal} onClose={closeModal} /> */}
-      {/* <RegistrationForm active={showModal} onClose={closeModal} /> */}
-
-      {/* <FriendInvitation active={showModal} onClose={closeModal} /> */}
-      {/* <DetailedInformation active={showModal} onClose={closeModal} /> */}
-      <ExpenseAndIncomeWindow active={showModal} onClose={closeModal} />
-    </div>
+    <>
+      <MainTarget />
+      <MenuItem />
+    </>
   );
 }
+
+export default withLayout(Home);
