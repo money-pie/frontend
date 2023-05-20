8119d0d211c591511edf5ec1ef8eaa122b545824
userkatrishaa <borekat36@gmail.com>
2023-05-20 17:00:07 +0300

    fix: update layout

--- layout/Layout.props.ts
+++ layout/Layout.props.ts
@@ -2,4 +2,5 @@ import { ReactNode } from "react";
 
 export interface LayoutProps {
   children: ReactNode;
+  visible: "visible" | "hidden";
 }
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,19 +4,22 @@ import { LayoutProps } from "./Layout.props";
 import styles from "./Layout.module.css";
 import { Header } from "./Header/Header";
 
-function Layout({ children }: LayoutProps): JSX.Element {
+function Layout({ children, visible }: LayoutProps): JSX.Element {
   return (
     <div className={styles.wrapper}>
-      <Header visible="hidden" className={styles.header} />
+      <Header visible={visible} className={styles.header} />
       <div className={styles.body}>{children}</div>
     </div>
   );
 }
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) =>
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>,
+  visible: "visible" | "hidden"
+) =>
   function withLayoutComponent(props: T): JSX.Element {
     return (
-      <Layout>
+      <Layout visible={visible}>
         <Component {...props} />
       </Layout>
     );
