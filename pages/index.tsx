0d986369b059cc110b34380410ac5dcf7182bfa9
userkatrishaa <borekat36@gmail.com>
2023-05-22 13:13:40 +0300

    fix: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,10 @@ import DemoMode from "../page-components/DemoMode/DemoMode";
 import PlusIcon from "../page-components/PlusIcon/plusIcon.svg";
 import styles from "../page-components/MainTarget/PlusIcon.module.css";
 import ExpenseAndIncomeWindow from "../components/modules/ExpenseAndIncomeWindow/ExpenseAndIncomeWindow";
+import MainComponent from "../page-components/MainComponent/MainComponent";
 
 function Home(): JSX.Element {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // пользователь зарегистрирован или нет
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // пользователь зарегистрирован или нет
 
   const [showExpenseIncomeWindow, setShowExpenseIncomeWindow] = useState<boolean>(false);
 
@@ -27,7 +28,30 @@ function Home(): JSX.Element {
     <>
       <MainTarget />
 
-      <MenuItem />
+      <MenuItem>
+        <MainComponent title="Развлечения" sum={1200}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Продукты" sum={3479}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Зарплата" sum={15000}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Транспорт" sum={118}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Продукты" sum={1000}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Продажа" sum={2450}>
+          19 марта 2023
+        </MainComponent>
+        <MainComponent title="Здоровье" sum={1200}>
+          19 марта 2023
+        </MainComponent>
+      </MenuItem>
+
       {!isLoggedIn ? null : (
         <div>
           <span
