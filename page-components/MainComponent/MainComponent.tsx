f23777c364b91bf8ed871c3c1ca48503a50c8661
userkatrishaa <borekat36@gmail.com>
2023-05-22 13:00:13 +0300

    feat: add main page component

---
+++ page-components/MainComponent/MainComponent.tsx
@@ -0,0 +1,142 @@
+import cn from "classnames";
+import React from "react";
+import { MainComponentProps } from "./MainComponent.props";
+import styles from "./MainComponent.module.css";
+import Htag from "../../components/elements/Htag/Htag";
+
+function MainComponent({
+  className,
+  children,
+  title,
+  sum,
+  ...props
+}: MainComponentProps): JSX.Element {
+  interface IconData {
+    icon: JSX.Element;
+    color: string;
+  }
+
+  const getIconByTitle = (category: string): IconData => {
+    const titleLowercase = category.toLowerCase();
+
+    if (titleLowercase.includes("продукты")) {
+      return {
+        icon: <img src="/images/icon1.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("здоровье")) {
+      return {
+        icon: <img src="/images/icon2.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("развлечения")) {
+      return {
+        icon: <img src="/images/icon3.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("жкх")) {
+      return {
+        icon: <img src="/images/icon4.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("обучение")) {
+      return {
+        icon: <img src="/images/icon5.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("фитнес")) {
+      return {
+        icon: <img src="/images/icon6.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("транспорт")) {
+      return {
+        icon: <img src="/images/icon7.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    if (titleLowercase.includes("налоги")) {
+      return {
+        icon: <img src="/images/icon8.svg" alt="Icon" />,
+        color: "var(--complement-weak)",
+      };
+    }
+
+    // Для поступлений
+
+    if (titleLowercase.includes("зарплата")) {
+      return {
+        icon: <img src="/images/icon1income.svg" alt="Icon" />,
+        color: "var(--accent-strong)",
+      };
+    }
+
+    if (titleLowercase.includes("награда")) {
+      return {
+        icon: <img src="/images/icon2income.svg" alt="Icon" />,
+        color: "var(--accent-strong)",
+      };
+    }
+
+    if (titleLowercase.includes("подарок")) {
+      return {
+        icon: <img src="/images/icon3income.svg" alt="Icon" />,
+        color: "var(--accent-strong)",
+      };
+    }
+
+    if (titleLowercase.includes("продажа")) {
+      return {
+        icon: <img src="/images/icon4income.svg" alt="Icon" />,
+        color: "var(--accent-strong)",
+      };
+    }
+
+    if (titleLowercase.includes("другое")) {
+      return {
+        icon: <img src="/images/icon5income.svg" alt="Icon" />,
+        color: "var(--accent-strong)",
+      };
+    }
+
+    // Иконка по умолчанию, если заголовок не совпал ни с одним условием
+
+    return {
+      icon: <img src="/images/icondefault.svg" alt="Icon" />,
+      color: "var(--accent-weak)",
+    };
+  };
+
+  const iconData = getIconByTitle(title);
+  const { icon, color } = iconData;
+
+  return (
+    <div className={cn(className, styles["main-component"])} {...props}>
+      <div className={styles["space-row"]}>
+        <div className={styles["icon-size"]}>{icon}</div>
+        <div className={styles.column}>
+          <Htag className={styles.h3} tag="h3">
+            {title}
+          </Htag>
+          <p>{children}</p>
+        </div>
+        <span style={{ color }}>{color === "var(--accent-strong)" ? `+${sum}₽` : `-${sum}₽`}</span>
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
