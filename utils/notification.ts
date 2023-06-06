e51be56293bd4c45c8d560476db5fded9a396449
pustart <polaris9955@gmail.com>
2023-06-06 13:56:49 +0300

    feat: add notification utils

--- utils/notification.ts
+++ utils/notification.ts
@@ -4,5 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 export const showNotification = (message: string | undefined, type: "success" | "error") => {
   if (type === "error" && message !== undefined) {
     toast.error(message);
+  } else if (type === "success" && message !== undefined) {
+    toast.success(message);
   }
 };
